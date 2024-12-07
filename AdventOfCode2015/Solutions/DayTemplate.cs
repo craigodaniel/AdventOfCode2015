d12db55ebbdcdd2404f4f081d98cb3950f7a6b9e
Craig O'Daniel <craig.odaniel@gmail.com>
2024-12-07 17:49:16 -0500

    Created template class for day solution

---
+++ AdventOfCode2015/Solutions/DayTemplate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2015.Solutions
+{
+    public class DayTemplate
+    {
+        // --- Day Template:  ---
+        // https://adventofcode.com/2015/day/Template
+        //
+        // Part 1 runtime:
+        // Part 2 runtime:
+        //
+        // Comments: 
+
+        private static string fileDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\AocInputs";
+        private static string fileName = "dayTemplate_sample.txt";
+        //private static string fileName = "dayTemplate_actual.txt";
+        private static string[] lines = File.ReadAllLines(fileDir + "\\" + fileName);
+
+        public static void Part1()
+        {
+            long startTime = Stopwatch.GetTimestamp();
+            int answer = 0;
+
+            TimeSpan elapsedTime = Stopwatch.GetElapsedTime(startTime);
+            Console.WriteLine("Part 1 runtime: {0}ms. The answer is: {1}", elapsedTime.TotalMilliseconds, answer);
+        }
+
+        public static void Part2()
+        {
+            long startTime = Stopwatch.GetTimestamp();
+            int answer = 0;
+
+            TimeSpan elapsedTime = Stopwatch.GetElapsedTime(startTime);
+            Console.WriteLine("Part 2 runtime: {0}ms. The answer is: {1}", elapsedTime.TotalMilliseconds, answer);
+        }
+    }
+}
