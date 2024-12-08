cbb5db6242db1f3024139638fc90c0e8cbdc9cdf
Craig O'Daniel <craig.odaniel@gmail.com>
2024-12-07 19:12:24 -0500

    Created AocLib. Wrote function to print 2d arrays to console.

---
+++ AdventOfCode2015/AocLibrary2015/AocLib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2015.AocLibrary2015
+{
+    public class AocLib
+    {
+        public static void Print2dIntArray(int[,] array)
+        {
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    sb.Append(array[row, col].ToString() + " ");
+                }
+
+                Console.WriteLine(sb.ToString());
+            }
+        }
+    }
+}
